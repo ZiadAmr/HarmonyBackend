12ae1955b3c38637bcea82508b02add00106c0fd
ZiadAmr <ziad2ziad2@gmail.com>
2024-08-07 13:54:52 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+// endpoints
+// get test
+func getTest(c *gin.Context) {
+	fmt.Println("Recieved GET /test")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	router := gin.Default()
+	router.GET("/test", getTest)
+
+	router.Run("localhost:8080")
+}
